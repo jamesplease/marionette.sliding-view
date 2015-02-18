632bd95d2473af65e58ddd648d3d3122cbf663b8
Jmeas <jellyes2@gmail.com>
2015-02-18 11:26:13 -0500

    First work.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # marionette.sliding-view
+[![Travis build status](http://img.shields.io/travis/jmeas/marionette.sliding-view.svg?style=flat)](https://travis-ci.org/jmeas/marionette.sliding-view)
+[![Code Climate](https://codeclimate.com/github/jmeas/marionette.sliding-view/badges/gpa.svg)](https://codeclimate.com/github/jmeas/marionette.sliding-view)
+[![Test Coverage](https://codeclimate.com/github/jmeas/marionette.sliding-view/badges/coverage.svg)](https://codeclimate.com/github/jmeas/marionette.sliding-view)
+[![Dependency Status](https://david-dm.org/jmeas/marionette.sliding-view.svg)](https://david-dm.org/jmeas/marionette.sliding-view)
+[![devDependency Status](https://david-dm.org/jmeas/marionette.sliding-view/dev-status.svg)](https://david-dm.org/jmeas/marionette.sliding-view#info=devDependencies)
+
 A simple sliding Collection View in Marionette.
+
---
+++ src/marionette.sliding-view.js
@@ -0,0 +1,105 @@
+import Backbone from 'backbone';
+import Mn from 'backbone.marionette';
+
+Mn.SlidingView = Mn.CollectionView.extend({
+  constructor(options = {}) {
+
+    // Throw an error if passed a `collection` option. SlidingView
+    // does not follow the same API as CollectionView, so we want
+    // to catch errors early on
+    if (options.collection) {
+      throw new Error('Do not pass a `collection` option to SlidingView.');
+    }
+
+    // Store the referenceCollection on the SlidingView
+    this.referenceCollection = options.referenceCollection;
+
+    // Set the collection to be an empty collection,
+    // and then delete the collection passed in as an
+    // option, if one was
+    this.collection = new Backbone.Collection();
+
+    Mn.CollectionView.prototype.constructor.apply(this, arguments);
+
+    // Get our initial boundaries, and then update the collection
+    this.lowerBound = this.getInitialLowerBound(...arguments);
+    this.upperBound = this.getInitialUpperBound(...arguments);
+    this._updateCollection();
+  },
+
+  // Register the event that calls the onScroll method. The default
+  // is to listen to the view's own scroll event, but it could just
+  // as easily listen to another element's scroll event, too.
+  registerSlideEvent() {
+    this.$el.on('scroll', this.onScroll);
+  },
+
+  // The scroll event is throttled at 60fps. It is recommended
+  // that you use requestAnimationFrame here, if you can, for
+  // a noticeable performance boost.
+  onScroll() {
+    window.setTimeout(() => {
+      this._onScroll();
+    }, 1000/60);
+  },
+
+  // Called at 60fps within the scroll handler
+  _onScroll() {
+
+    // Pass along our arguments to the methods that calculate our boundaries
+    var lowerBound = this.getLowerBound(...arguments);
+    var upperBound = this.getUpperBound(...arguments);
+
+    // We need to render if either of the boundaries have changed. If this is
+    // the case, and there's already a render in the queue, then we cancel out
+    // that queued render. This prevents users who are scrolling very fast
+    // from getting too many renders at once. It won't render until they've
+    // slowed down a bit.
+    if (lowerBound !== this.lowerBound || upperBound !== this.upperBound) {
+      if (this._deferredUpdateId) {
+        clearTimeout(this._deferredUpdateId);
+      }
+    }
+
+    // If the boundaries are unchanged, then we bail out early
+    if (lowerBound === this.lowerBound && upperBound === this.upperBound) {
+      return;
+    }
+
+    // Update our indices
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+
+    // Defer an update for 50ms. This prevents many renders when scrolling fast.
+    var self = this;
+    this._deferredUpdateId = setTimeout(function() {
+      self._updateCollection();
+    }, 50);
+  },
+
+  // Get our initial boundaries
+  getInitialLowerBound() {},
+  getInitialUpperBound() {},
+
+  // The methods that determine our boundaries with each
+  // 'update' (typically the scroll event)
+  getLowerBound() {},
+  getUpperBound() {},
+
+  // Use the boundaries calculated in `onScroll` to filter down
+  // your collection to only the ones that you wish to show. Return
+  // an array of models to be set on the collection. The default is
+  // to just return all of the models
+  filterCollection() {
+    return this.referenceCollection.models;
+  },
+
+  // Update the collection with the results of `filterCollection`
+  // This leverages two important facts:
+  // 1. Collection#set performs a 'smart' update at the data layer
+  // 2. CollectionView performs a 'smart' update of the view layer
+  //    whenever the data layer changes
+  _updateCollection() {
+    this.collection.set(this.filterCollection());
+  }
+});
