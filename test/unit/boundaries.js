2c3e2f5fb989fc8cc163e6dbe756373b5fe69f27
Jmeas <jellyes2@gmail.com>
2015-02-18 14:56:00 -0500

    Add boundary tests.

--- test/unit/boundaries.js
+++ test/unit/boundaries.js
@@ -1,6 +1,6 @@
 var SlidingView, slidingView, collection, clock;
 
-describe('When boundary methods are specified, and a scroll event is fired', () => {
+describe('When boundary methods are specified, and the throttled method is called', () => {
   beforeEach(() => {
     clock = sinon.useFakeTimers();
 
@@ -26,8 +26,6 @@ describe('When boundary methods are specified, and a scroll event is fired', () 
     });
 
     spy(slidingView, '_updateCollection');
-
-    slidingView.$el.scroll();
   });
 
   afterEach(() => {
@@ -35,6 +33,10 @@ describe('When boundary methods are specified, and a scroll event is fired', () 
   });
 
   describe('and no time has passing', () => {
+    beforeEach(() => {
+      slidingView.throttledScrollHandler();
+    });
+
     it('should not update the collection', () => {
       expect(slidingView._updateCollection).to.not.have.been.called;
     });
@@ -42,6 +44,7 @@ describe('When boundary methods are specified, and a scroll event is fired', () 
 
   describe('and 20ms has passed', () => {
     beforeEach(() => {
+      slidingView.throttledScrollHandler();
       clock.tick(20);
     });
 
@@ -50,12 +53,59 @@ describe('When boundary methods are specified, and a scroll event is fired', () 
     });
   });
 
-  describe('and 51ms has passed', () => {
+  describe('and 50ms has passed', () => {
     beforeEach(() => {
-      clock.tick(67);
+      slidingView.throttledScrollHandler();
+      clock.tick(50);
     });
 
-    it('should update the collection', () => {
+    it('should call the update collection method', () => {
+      expect(slidingView._updateCollection).to.have.been.calledOnce;
+    });
+
+    it('should set the collection correctly', () => {
+      expect(slidingView.collection.at(0)).to.deep.equal(collection.at(0));
+    });
+  });
+
+  describe('and the callback is called quickly, but the boundaries do not change', () => {
+    beforeEach(() => {
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      clock.tick(50);
+    });
+
+    it('should only update the collection once', () => {
+      expect(slidingView._updateCollection).to.have.been.calledOnce;
+    });
+  });
+
+  describe('and the boundaries change, but the callback is called too quickly', () => {
+    beforeEach(() => {
+      var count = 0;
+      slidingView.getLowerBound = function() {
+        return ++count;
+      };
+      slidingView.getUpperBound = function() {
+        return ++count;
+      };
+
+      spy(global, 'clearTimeout');
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      slidingView.throttledScrollHandler();
+      clock.tick(50);
+    });
+
+    it('should clear the timeout on all but one of them (n-1)', () => {
+      expect(global.clearTimeout).to.have.callCount(4);
+    });
+
+    it('should only update the collection once', () => {
       expect(slidingView._updateCollection).to.have.been.calledOnce;
     });
   });
